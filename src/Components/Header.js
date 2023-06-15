5b96a281c3b4e96bb3323e707323fa9400f39606
jezinho22 <jez.johns@gmail.com>
2023-06-15 10:29:07 +0100

    added menu

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,28 @@
 import { useState } from "react";
 import Form from "./Form";
+import burgerMenu from "../Resources/burger-bar.png"
+import Menu from "./Menu";
 
 export default function Header({ handleSubmit, handleChange, form }) {
 	const [showForm, setshowForm] = useState(false);
+	const [showMenu, setShowMenu] = useState(false);
 	function handleShowForm() {
 		setshowForm(!showForm);
+	}
+	function handleShowMenu (){
+		setShowMenu(!showMenu)
 	}
 	return (
 		<header>
 			<div className="headerRow">
 				<h1>Joe's Student Drinking App</h1>
-				<button id="addName" onClick={handleShowForm}>
+				<img src={burgerMenu} alt="hamburger menu icon" onClick={handleShowMenu}></img>
+				{showMenu && (
+					<Menu showForm={handleShowForm}/>
+				)}
+				{/* <button id="addName" onClick={handleShowForm}>
 					Add item
-				</button>
+				</button> */}
 			</div>
 			{showForm && (
 				<Form
---
+++ src/Components/Menu.js
@@ -0,0 +1,13 @@
+
+export default function Menu() {
+  return (
+    <div>
+        <ul>
+            <a href="#"><li>About alcohol</li></a>
+            <a href="#"><li>About this app</li></a>
+            <a href="#"><li>View drinks</li></a>
+            <a href="#"><li>Add drinks</li></a>
+        </ul>
+    </div>
+  )
+}
