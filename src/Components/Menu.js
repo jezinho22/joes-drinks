8564d9c3ef44960b13ff23f4f1e308d9f3b82368
Jez <jeremy.johns@techeducators.co.uk>
2023-06-19 21:05:02 +0100

    animated menu

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,12 +1,12 @@
 
-export default function Menu({showMenu,handleShowMenu}) {
+export default function Menu({showMenu,handleShowMenu, handleShowForm}) {
   return (
     <div className={`menu ${showMenu}`} onClick={()=>handleShowMenu("menu-close")}>
         <ul>
             <a href="#" ><li>About alcohol</li></a>
             <a href="#" ><li>About this app</li></a>
             <a href="#" ><li>View drinks</li></a>
-            <a href="#" ><li>Add drinks</li></a>
+            <a href="#" onClick={handleShowForm}><li>Add drinks</li></a>
         </ul>
     </div>
   )
