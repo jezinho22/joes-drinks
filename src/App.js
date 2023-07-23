11b28cdd63c32fcb4508397c1e7d7f059a81bc14
jezinho22 <jez.johns@gmail.com>
2023-07-23 16:37:30 +0100

    not working - moving functions and states to app

--- src/App.js
+++ src/App.js
@@ -8,35 +8,7 @@ import Footer from "./Components/Footer/Footer";
 import { useEffect, useState } from "react";
 
 function App() {
-	// localStorage.clear();
-	const [form, setForm] = useState({
-		drinkItem: "",
-		volume: "",
-		alcohol: "",
-		price: "",
-	});
-	const [cards, setCards] = useState([
-		{
-			drinkItem: "Aldi pinot grigio",
-			volume: 750,
-			alcohol: 11.5,
-			price: 3.99,
-		},
-		{
-			drinkItem: "Stella Artois",
-			volume: 2272,
-			alcohol: 4.6,
-			price: 5.75,
-		},
-	]);
-	// useEffect only runs on page load - or on something specified in second argument
-	useEffect(() => {
-		// check local storage for cards
-		const localCards = localStorage.getItem("drinksStorage");
-		if (localCards) {
-			setCards(JSON.parse(localCards));
-		}
-	}, []);
+
 	function callSetStateForm() {
 		setForm({
 			drinkItem: "",
@@ -48,26 +20,36 @@ function App() {
 	// updating form inputs
 	function handleChange(event) {
 		const newForm = { ...form, [event.target.name]: event.target.value };
-		// if (newForm.multipack) {
-		// 	newForm.volume = newForm.multiCount * newForm.multiVolume;
-		// }
-		// console.log(newForm);
 		setForm(newForm);
 	}
 	// suubmit button for form
 	function handleSubmit(event) {
 		event.preventDefault();
-		console.log(form);
 		// quick workaround to push to cards because setCards won't push
 		// there must be an easier way
 		const x = [...cards];
 		x.push(form);
 		setCards(x);
-		// const clearForm = { ...form, multiVolume: 0, multiCount: 0 };
-		// setForm(clearForm);
 		// update local storage with new card
 		localStorage.setItem("drinksStorage", JSON.stringify(cards));
 	}
+
+		const [showForm, setshowForm] = useState(false);
+		const [showMenu, setShowMenu] = useState("menu-close");
+
+	function handleShowForm() {
+		setshowForm(!showForm);
+		if (showForm){ 
+		setShowMenu("menu-close");
+		callSetStateForm();
+		}
+	}	
+
+	function handleShowMenu(instruction) {
+		setShowMenu(instruction);
+	}
+
+
 
 	return (
 		<div>
@@ -76,8 +58,11 @@ function App() {
 				handleChange={handleChange}
 				form={form}
 				callSetStateForm={callSetStateForm}
+				handleShowForm={handleShowForm}
+				handleShowMenu={handleShowMenu}
+				
 			/>
-			<Main cards={cards} form={form} />
+			<Main cards={cards}/>
 			<Footer />
 		</div>
 	);
