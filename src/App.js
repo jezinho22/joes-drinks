95557596afa130522ac9b4ccad1695f155138b72
jezinho22 <jez.johns@gmail.com>
2023-08-05 11:03:31 +0100

    animated form

--- src/App.js
+++ src/App.js
@@ -5,64 +5,34 @@ import "./App.css";
 import Header from "./Components/Header/Header";
 import Main from "./Components/Main/Main";
 import Footer from "./Components/Footer/Footer";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function App() {
 
-	function callSetStateForm() {
-		setForm({
-			drinkItem: "",
-			volume: "",
-			alcohol: "",
-			price: "",
-		});
-	}
-	// updating form inputs
-	function handleChange(event) {
-		const newForm = { ...form, [event.target.name]: event.target.value };
-		setForm(newForm);
-	}
-	// suubmit button for form
-	function handleSubmit(event) {
-		event.preventDefault();
-		// quick workaround to push to cards because setCards won't push
-		// there must be an easier way
-		const x = [...cards];
-		x.push(form);
-		setCards(x);
-		// update local storage with new card
-		localStorage.setItem("drinksStorage", JSON.stringify(cards));
-	}
-
-		const [showForm, setshowForm] = useState(false);
+		const [showForm, setshowForm] = useState("close");
 		const [showMenu, setShowMenu] = useState("menu-close");
 
-	function handleShowForm() {
-		setshowForm(!showForm);
-		if (showForm){ 
-		setShowMenu("menu-close");
-		callSetStateForm();
-		}
+	function handleShowForm(action) {
+		console.log(action)
+		setshowForm(action);
+
 	}	
 
 	function handleShowMenu(instruction) {
 		setShowMenu(instruction);
 	}
 
-
-
 	return (
 		<div>
 			<Header
-				handleSubmit={handleSubmit}
-				handleChange={handleChange}
-				form={form}
-				callSetStateForm={callSetStateForm}
-				handleShowForm={handleShowForm}
 				handleShowMenu={handleShowMenu}
+				showMenu={showMenu}
+				handleShowForm={handleShowForm}
 				
 			/>
-			<Main cards={cards}/>
+			<Main 
+				handleShowForm = {handleShowForm}
+				showForm = {showForm}/>
 			<Footer />
 		</div>
 	);
--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -7,7 +7,7 @@ export default function Menu({showMenu,handleShowMenu, handleShowForm}) {
             <a href="#about-app" ><li>About this app</li></a>            
             <a href="#about-alcohol" ><li>About alcohol</li></a>
             <a href="#drink-cards" ><li>View drinks</li></a>
-            <a href="#" onClick={handleShowForm}><li>Add drinks</li></a>
+            <a href="#" onClick={()=>handleShowForm("open")}><li>Add drinks</li></a>
         </ul>
     </div>
   )
