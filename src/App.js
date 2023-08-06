89946d7aac1507e51257cbe12293dd7d19b4a2e8
jezinho22 <jez.johns@gmail.com>
2023-08-06 20:32:50 +0100

    fixed sort

--- src/App.js
+++ src/App.js
@@ -28,7 +28,6 @@ function App() {
 				handleShowMenu={handleShowMenu}
 				showMenu={showMenu}
 				handleShowForm={handleShowForm}
-				
 			/>
 			<Main 
 				handleShowForm = {handleShowForm}
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -13,6 +13,8 @@ export default function Main({handleShowForm, showForm}) {
 		volume: "",
 		alcohol: "",
 		price: "",
+		units:"",
+		pricePerUnit:""
 	});
 	const [cards, setCards] = useState([
 		{
@@ -28,12 +30,21 @@ export default function Main({handleShowForm, showForm}) {
 			price: 5.75,
 		},
 	]);
+	
 	// useEffect only runs on page load - or on something specified in second argument
 	useEffect(() => {
 		// check local storage for cards
 		const localCards = localStorage.getItem("drinksStorage");
 		if (localCards) {
 			setCards(JSON.parse(localCards));
+		} else {
+			// this needs to be done further down, during submit form
+			// instead of trying to mutate cards state
+			let newOnes = cards.map(card => {
+				return {...card, units:alcoholUnits(card), pricePerUnit:pricePerUnit(card)}
+			})
+			console.log(newOnes)
+			setCards(newOnes)
 		}
 	}, []);
 
@@ -50,34 +61,69 @@ export default function Main({handleShowForm, showForm}) {
 		const newForm = { ...form, [event.target.name]: event.target.value };
 		setForm(newForm);
 	}
+	function alcoholUnits(newCard) {
+		return ((newCard.volume * newCard.alcohol) / 1000).toFixed(1);
+	}
+	function pricePerUnit(newCard){
+		return (newCard.price / ((newCard.volume * newCard.alcohol) / 1000)).toFixed(2)
+	}
 	// suubmit button for form
 	function handleSubmit(event) {
 		event.preventDefault();
-		// quick workaround to push to cards because setCards won't push
-		// there must be an easier way
-		const x = [...cards];
-		x.push(form);
-		setCards(x);
+		// make form into a new card and add to cards
+		// add calculations to it
+		let tempForm = {...form, units:alcoholUnits(form),pricePerUnit:pricePerUnit(form)}
+		console.log(form)
+		console.log(tempForm)
+		setCards([...cards, {...tempForm}]);
+
+		closeForm();
+
+		// reset form
+		event.target.reset();
+
+	}
+	function closeForm(){
 		// update local storage with new card
-		localStorage.setItem("drinksStorage", JSON.stringify(cards));
+		localStorage.setItem("drinksStorage", JSON.stringify(cards));		
+
 		// this may not work - reset form or directly use set state here
 		handleShowForm("close")
 		callSetStateForm()
-		// moved submit from button to form to allow this reset
-		event.target.reset()
+		// scroll down to view cards
 		const viewCards = document.querySelector('#drink-cards')
 		viewCards.scrollIntoView({ behavior: 'smooth', block: 'center' })
 	}
-	// function sortByPrice(){
-	// 	let sorted = cards.sort((a, b)=> a.price - b.price)
-
-	// }
-
+	function handleSort(sortBy){
+		console.log(sortBy)
+		let sorted = [...cards]
+		if (sortBy == "drinkItem"){
+			setCards(alphaSort(sorted))
+		} else {
+			sorted.sort((a, b)=> a[sortBy] - b[sortBy])
+		setCards(sorted)		
+		}
+	}
+	
+	function alphaSort(array){
+		array.sort((a, b) => {
+			const nameA = a.drinkItem.toUpperCase(); // ignore upper and lowercase
+			const nameB = b.drinkItem.toUpperCase(); // ignore upper and lowercase
+			if (nameA < nameB) {
+			  return -1;
+			}
+			if (nameA > nameB) {
+			  return 1;
+			}
+			// names must be equal
+			return 0;
+		  });
+		return array
+	}
 	return (
 		<div className = "Main">
 			<AboutApp/>
 			<AboutDrink/>
-			{/* remove cond render and instead reveal with animation */}
 			<Form
 					handleSubmit={handleSubmit}
 					handleChange={handleChange}
@@ -86,9 +132,9 @@ export default function Main({handleShowForm, showForm}) {
 					handleShowForm={handleShowForm}></Form>
 			<div id="drink-cards"></div>
 			<div className="buttons">
-				<button id="sortPricePerUnit">Sort by price per unit</button>
-				<button id="sortPrice">Sort by price</button>
-				<button className="sortAlpha">Sort by name</button>
+				<button id="sortPricePerUnit" onClick={()=>handleSort('pricePerUnit')}>Sort by price per unit</button>
+				<button id="sortPrice" onClick={()=>handleSort('price')}>Sort by price</button>
+				<button id="sortDrinkItem" onClick={()=>handleSort('drinkItem')}>Sort by name</button>
 			</div>
 			<div className="cards-container">
 				{cards.map((card, index) => (
