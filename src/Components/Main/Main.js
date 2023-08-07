3767e841322a62806568ba085da451398804a2e1
Jez <jeremy.johns@techeducators.co.uk>
2023-08-07 20:57:16 +0100

    fixed pints broke rest

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -16,6 +16,7 @@ export default function Main({handleShowForm, showForm}) {
 		units:"",
 		pricePerUnit:""
 	});
+
 	const [cards, setCards] = useState([
 		{
 			drinkItem: "Aldi pinot grigio",
@@ -30,24 +31,60 @@ export default function Main({handleShowForm, showForm}) {
 			price: 5.75,
 		},
 	]);
-	
+
 	// useEffect only runs on page load - or on something specified in second argument
 	useEffect(() => {
 		// check local storage for cards
+		console.log(cards)
 		const localCards = localStorage.getItem("drinksStorage");
 		if (localCards) {
 			setCards(JSON.parse(localCards));
-		} else {
-			// this needs to be done further down, during submit form
-			// instead of trying to mutate cards state
-			let newOnes = cards.map(card => {
-				return {...card, units:alcoholUnits(card), pricePerUnit:pricePerUnit(card)}
-			})
-			console.log(newOnes)
-			setCards(newOnes)
+		}  else {
+			setCards(addCalculatedValues())
+
+			localStorage.setItem('localCards', JSON.stringify(cards))
+
 		}
 	}, []);
 
+	function addCalculatedValues(){
+		let newOnes = cards.map(card => {
+			return {...card, units:alcoholUnits(card), pricePerUnit:pricePerUnit(card), pintEquivalent:equivalentPints(card)}
+		})
+		return newOnes
+	}
+	// ### calculations for card values ### 
+	function alcoholUnits(newCard) {
+		console.log("units")
+		return ((newCard.volume * newCard.alcohol) / 1000).toFixed(1);
+	}
+	function pricePerUnit(newCard){
+		return (newCard.price / ((newCard.volume * newCard.alcohol) / 1000)).toFixed(2)
+	}
+	function equivalentPints(newCard) {
+		// create an array of image attributes to display
+		let pintsArray = []
+		// work out whole pints
+		let pints = Math.floor(newCard.units / 2.2);
+		for (let i = 0; i < pints; i++) {
+			pintsArray.push({
+				src: "../Resources/pint.png",
+				alt: "pint-glass",
+				height: 24,
+			});
+		}
+		// work out remainder as proportion of 24px-high pint
+		const pintHeight = 24;
+		let partPint = (pintHeight * (newCard.units % 2.2)) / 2.2;
+		pintsArray.push({
+			  src: "../Resources/pint.png", 
+			  alt: "pint-glass", 
+			  height: `${partPint}px` 
+			});
+		return pintsArray
+	}
+
+	// reset form state  - is this really needed? won't reset form reset this as well?
 	function callSetStateForm() {
 		setForm({
 			drinkItem: "",
@@ -61,21 +98,16 @@ export default function Main({handleShowForm, showForm}) {
 		const newForm = { ...form, [event.target.name]: event.target.value };
 		setForm(newForm);
 	}
-	function alcoholUnits(newCard) {
-		return ((newCard.volume * newCard.alcohol) / 1000).toFixed(1);
-	}
-	function pricePerUnit(newCard){
-		return (newCard.price / ((newCard.volume * newCard.alcohol) / 1000)).toFixed(2)
-	}
-	// suubmit button for form
+
+	// submit button for form
 	function handleSubmit(event) {
 		event.preventDefault();
 		// make form into a new card and add to cards
 		// add calculations to it
 		let tempForm = {...form, units:alcoholUnits(form),pricePerUnit:pricePerUnit(form)}
-		console.log(form)
-		console.log(tempForm)
-		setCards([...cards, {...tempForm}]);
+		let pintsEquivalent = equivalentPints(form)
+		// need units to then do pintEquivalent
+		setCards([...cards, {...tempForm, pintEquivalent:pintsEquivalent}]);
 
 		closeForm();
 
@@ -84,16 +116,15 @@ export default function Main({handleShowForm, showForm}) {
 
 	}
 	function closeForm(){
-		// update local storage with new card
-		localStorage.setItem("drinksStorage", JSON.stringify(cards));		
-
 		// this may not work - reset form or directly use set state here
 		handleShowForm("close")
-		callSetStateForm()
+		// callSetStateForm()
 		// scroll down to view cards
 		const viewCards = document.querySelector('#drink-cards')
 		viewCards.scrollIntoView({ behavior: 'smooth', block: 'center' })
+		// update local storage with new card
 	}
+
 	function handleSort(sortBy){
 		console.log(sortBy)
 		let sorted = [...cards]
@@ -104,7 +135,7 @@ export default function Main({handleShowForm, showForm}) {
 		setCards(sorted)		
 		}
 	}
-	
+
 	function alphaSort(array){
 		array.sort((a, b) => {
 			const nameA = a.drinkItem.toUpperCase(); // ignore upper and lowercase
