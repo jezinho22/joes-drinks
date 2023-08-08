51d33a6e4df9feaaa74e8aeef7156d7fcc06b5c8
jezinho22 <jez.johns@gmail.com>
2023-08-08 08:20:44 +0100

    local storage issue moved calcs to handlechange

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -4,6 +4,31 @@ import { useState } from "react";
 import pintPot from "../../Resources/pint.png";
 
 export default function Card({ cardData, cardIndex }) {
+const [pints, setPints] = useState(equivalentPints(cardData))
+
+	function equivalentPints(newCard) {
+		// create an array of image attributes to display
+		let pintsArray = []
+		// work out whole pints
+		let pints = Math.floor(newCard.units / 2.2);
+		for (let i = 0; i < pints; i++) {
+			pintsArray.push({
+				src: "../Resources/pint.png",
+				alt: "pint-glass",
+				height: 24,
+			});
+		}
+		// work out remainder as proportion of 24px-high pint
+		const pintHeight = 24;
+		let partPint = (pintHeight * (newCard.units % 2.2)) / 2.2;
+		pintsArray.push({
+			  src: "../Resources/pint.png", 
+			  alt: "pint-glass", 
+			  height: `${partPint}px` 
+			});
+		return pintsArray
+	}
+
 
 	return (
 		<div className="card" key={cardIndex}>
@@ -16,7 +41,7 @@ export default function Card({ cardData, cardIndex }) {
 			<h4>Price per unit: {cardData.pricePerUnit}</h4>
 
 			<div className = 'pints'>
-			{cardData.pintEquivalent.map((item, index) => (
+			{pints.map((item, index) => (
 				
 				<img src={pintPot} alt={item.alt} key={index} style={{ height : item.height }}/> ))}
 			</div>
